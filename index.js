b69dbbc880c696143d8e8a2a9dcd36391a3468ed
Matias Peñas <mattff3@gmail.com>
2020-07-27 00:48:01 -0300

    Changing job

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ bot.start((ctx) => {
     //console.log(ctx.updateSubTypes)
     ctx.reply('Welcome ' + ctx.from.first_name);
 
-    axios.get('https://rest.coinapi.io/v1/exchangerate/ETH/USD', {
-        headers:{
-            'X-CoinAPI-Key': process.env.COINAPI_KEY,
-            'Accept': 'application/json'
-        }
-    })
-        .then(response => {
-            //console.log(response.data);
-            var dateFormated = dayjs(response.data.time).format('MMMM D, YYYY h:mm A');
-            ctx.reply(`The Etherum value at ${dateFormated} is: ${response.data.rate}`);
-            Schedule.scheduleJob('*/30 * * * *', function(){
+    Schedule.scheduleJob('*/30 * * * *', function(){
+        axios.get('https://rest.coinapi.io/v1/exchangerate/ETH/USD', {
+            headers:{
+                'X-CoinAPI-Key': process.env.COINAPI_KEY,
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => {
+                //console.log(response.data);
+                var dateFormated = dayjs(response.data.time).format('MMMM D, YYYY h:mm A');
+                ctx.reply(`The Etherum value at ${dateFormated} is: ${response.data.rate}`);
                 ctx.reply(`The Etherum value at ${dateFormated} is: ${response.data.rate}`);
                 ctx.reply(response.data.rate);
             });
-        });
+    });
     
 });
 
