b301d156a722ded544f9d4f8f3f02bf33e5e8ee0
Matias Peñas <mattff3@gmail.com>
2020-08-10 14:51:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Etherum Telegram Bot
+
+Project to get the value of Etherum through Telegram. It's a cron running that sends the Etherum value (in USA Dollars)
+every 30 minutes.
+
+## Using the Application
+
+If you wanna use this app follow these instructions:
+
+* In telegram search for the following bot:
+`@silkhighwaybot`
+* Run the following command on the bot's chat:
+`/start`
+
+
+## Installing your own application
+
+### Prerequisites
+
+* Node
+* Npm
+
+
+### Instructions to run the app locally
+
+If you wanna run this application on your local:
+
+* Create a new Bot. For doing it go to Telegram and search for:
+`@BotFather`
+You should send a new message to the bot:
+`newbot`
+You should follow the instructions until you get the bot api credentials.
+* Create a new account on https://docs.coinapi.io/, get the api credentials
+* Add the credentials on the following env variable (check on the code):
+TELEGRAF_KEY
+COINAPI_KEY
--- index.js
+++ index.js
@@ -7,11 +7,7 @@ var dayjs = require('dayjs')
 const bot = new Telegraf(process.env.TELEGRAF_KEY);
 
 bot.start((ctx) => {
-    //Exploring ctx object:
-    //console.log(ctx.from)
-    //console.log(ctx.chat)
-    //console.log(ctx.message)
-    //console.log(ctx.updateSubTypes)
+
     ctx.reply('Welcome ' + ctx.from.first_name);
 
     Schedule.scheduleJob('*/30 * * * *', function(){
@@ -22,55 +18,11 @@ bot.start((ctx) => {
             }
         })
             .then(response => {
-                //console.log(response.data);
                 var dateFormated = dayjs(response.data.time).format('MMMM D, YYYY h:mm A');
                 ctx.reply(`The Etherum value at ${dateFormated} is: ${response.data.rate}`);
-                //ctx.reply(response.data.rate);
             });
     });
     
 });
 
-bot.help((ctx) => {
-    ctx.reply('Help');
-});
-
-bot.settings((ctx) => {
-    ctx.reply('Aca los settings');
-});
-
-// Create custom command
-bot.command(['mycommand', 'Mycommand', 'MyCommand'], (ctx) => {
-    ctx.reply('My Custom Command!!!');
-});
-
-bot.command(['opsididitagain', 'Opsididitagain'], (ctx) => {
-    ctx.reply('Do you wanna touch me?');
-});
-
-bot.command(["Imaslaveforyou", "Imaslaveforyou"], (ctx) => {
-    ctx.reply('tell me more, what do you allow me to do you?');
-});
-
-// Hear for some text
-bot.hears('computer', (ctx) => {
-    ctx.reply('Hey Im selling a computer');
-});
-
-// hear when user typing
-/*
-bot.on('text', ctx => {
-    ctx.reply('You are typing text');
-});
-*/
-
-bot.on('sticker', ctx => {
-    ctx.reply('Oh! you like the stickers');
-});
-
-
-
-
 bot.launch();
-
-
